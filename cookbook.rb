3303859b91c409f1aa36ef947930f2278afa7bbc
Rafael Kasinski <rakasin@gmail.com>
2022-10-16 15:51:26 -0300

    UYsed interpolation and variable

--- cookbook.rb
+++ cookbook.rb
@@ -1,6 +1,8 @@
 puts 'Bem-vindo ao Cookbook.'
 
 puts 'Digite o nome da sua receita.'
-gets
+nome = gets.chomp
 
 puts 'Receita cadastrada com sucesso!'
+
+puts "Receita de #{nome} cadastrada com sucesso!"
