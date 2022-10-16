b91c9dbe7fb33d9bd89849c0c0409b62c6922888
Rafael Kasinski <rakasin@gmail.com>
2022-10-16 16:12:01 -0300

    Finished array video

--- cookbook.rb
+++ cookbook.rb
@@ -1,8 +1,24 @@
+receitas = []
+
 puts 'Bem-vindo ao Cookbook.'
 
 puts 'Digite o nome da sua receita.'
 nome = gets.chomp
 
-puts 'Receita cadastrada com sucesso!'
+receitas << nome
 
 puts "Receita de #{nome} cadastrada com sucesso!"
+
+# receitas << 'Bolo de cenoura'
+
+# receitas << 3
+
+# receitas << 'suco'
+
+# puts receitas
+
+# receitas.delete(3)
+
+# puts receitas
+
+# puts receitas[0]
