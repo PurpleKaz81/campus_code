d73843b22d950c23c2036b2b43b022d6fd53effb
Rafael Kasinski <rakasin@gmail.com>
2022-10-16 19:04:53 -0300

    Established attr reader and accessors

--- conta.rb
+++ conta.rb
@@ -1,21 +1,11 @@
 class Conta
+  attr_accessor :titular, :saldo
+  attr_reader :numero
+
   def initialize(numero, titular, saldo)
     @numero = numero
     @titular = titular
     @saldo = saldo
     @tipo = 'corrente' # no need to fill in as parameter
   end
-
-  def numero
-    @numero
-  end
-
-  def titular
-    @titular
-  end
-
-  def saldo
-    @saldo
-  end
-
 end
