1314c007426a6981b7ffab2d99e83744311a15be
Rafael Kasinski <rakasin@gmail.com>
2022-10-16 23:31:05 -0300

    Extensive behavior for Conta and ContaCorrente now possible

--- conta_corrente.rb
+++ conta_corrente.rb
@@ -7,4 +7,21 @@ class ContaCorrente < Conta
     super(numero, titular, saldo)
     @limite = limite
   end
+
+  def sacar(valor)
+    if saldo + limite >= valor
+      self.saldo -= valor # we could also use @saldo
+    else
+      puts 'Se fodeu, man, não tem grana não!'
+    end
+  end
+
+  def transferir(conta_destino, valor)
+    if self.saldo + limite >= valor
+      sacar(valor) # self.sacar is optional cuz there's no = sign
+      conta_destino.depositar(valor)
+    else
+      puts 'Não dá pra transferir grana que você não tem, campeão!'
+    end
+  end
 end
